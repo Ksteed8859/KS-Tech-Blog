27cf246ee094ee475ba12f1f76ae96bdcd7bbe5c
Kate Steed <sunfiresteed@yahoo.com>
2023-02-06 12:18:51 -0700

    added models and seeds

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const User = require('./user.js');
+const Post = require('./post.js');
+
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Post };
